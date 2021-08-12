219c39e7ee891c5a8e160d85a60f438eb3ff7a31
itsjustrobert <robertlove99999@gmail.com>
2021-08-12 13:59:45 -0500

    Almost done with the app.js need one variable from the othe .js files to create a new item in the cart -robert

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -7,15 +7,20 @@ const Cart = function(items) {
 };
 
 Cart.prototype.addItem = function(product, quantity) {
-  // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  // TODO: Fill in this instance method to create a new CartItem and add it to this site !
+  this.items.unshift();
 };
+
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  const localStorage = JSON.stringify(this.items);
+  // local storage
 };
 
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
+  this.items.splice(item, 1);
   // Note: You will have to decide what kind of parameter to pass in here!
 };
 
